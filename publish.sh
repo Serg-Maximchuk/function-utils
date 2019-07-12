6ec42ab28323b11bee8dbb1fb27ab91c7929ba41
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2019-07-12 15:05:28 +0300

    Added comments.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # release commands
+
 ./mvnw clean deploy
 ./mvnw nexus-staging:release
+
+# maybe you will have to create GPG keys - step 7 in https://dzone.com/articles/publish-your-artifacts-to-maven-central
