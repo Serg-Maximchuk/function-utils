6306b452a69815fddddd3b86ddb02145a4ce85ef
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2019-07-12 15:06:49 +0300

    Echoing publishing process.

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # release commands
 
+echo 'deploy...'
 ./mvnw clean deploy
+echo 'release...'
 ./mvnw nexus-staging:release
 
 # maybe you will have to create GPG keys - step 7 in https://dzone.com/articles/publish-your-artifacts-to-maven-central
