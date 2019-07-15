55f6fd6fca3ca580bf6ba5a95b5a9cc7f9bd4671
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2019-07-15 18:39:08 +0300

    - Introduced ThrowingDoubleToIntFunction.

--- README.md
+++ README.md
@@ -168,7 +168,6 @@ https://github.com/jOOQ/jOOL
 ### Coming soon:
 
 ```
-ThrowingDoubleToIntFunction
 ThrowingDoubleToLongFunction
 ThrowingIntBinaryOperator
 ThrowingIntConsumer
---
+++ src/main/java/utils/functions/ThrowingDoubleToIntFunction.java
@@ -0,0 +1,76 @@
+package utils.functions;
+
+import java.util.Objects;
+import java.util.function.DoubleToIntFunction;
+
+/**
+ * {@link DoubleToIntFunction} that may sneaky throw checked {@code Exception}.
+ * <p>
+ * This is a functional interface whose functional method
+ * is {@link #applyAsIntThrows(double)}.
+ *
+ * @see DoubleToIntFunction
+ * @see ThrowingFunction
+ */
+@FunctionalInterface
+public interface ThrowingDoubleToIntFunction extends DoubleToIntFunction {
+
+    /**
+     * Unwrap {@link ThrowingDoubleToIntFunction}.
+     *
+     * @param function the one to be unwrapped
+     * @return unwrapped {@link DoubleToIntFunction}
+     */
+    static DoubleToIntFunction unthrow(ThrowingDoubleToIntFunction function) {
+        return function;
+    }
+
+    /**
+     * Wrap input {@code function} as a throwing one.
+     *
+     * @param function the one to be wrapped
+     * @return wrapped {@link ThrowingDoubleToIntFunction}
+     * @throws NullPointerException if {@code function} is null
+     */
+    static ThrowingDoubleToIntFunction wrap(DoubleToIntFunction function) {
+        return Objects.requireNonNull(function)::applyAsInt;
+    }
+
+    /**
+     * Use it when you don't want to cast lambda by yourself.
+     *
+     * @param function the one to map and return
+     * @return the same function
+     */
+    static ThrowingDoubleToIntFunction map(ThrowingDoubleToIntFunction function) {
+        return function;
+    }
+
+
+    /**
+     * Applies this function to the given argument.
+     *
+     * @param value the function argument
+     * @return the function result
+     */
+    int applyAsIntThrows(double value) throws Exception;
+
+
+    @Override
+    default int applyAsInt(double value) {
+        try {
+            return applyAsIntThrows(value);
+        } catch (Exception e) {//noinspection RedundantTypeArguments
+            throw Functions.<RuntimeException>sneakyThrow(e);
+        }
+    }
+
+    /**
+     * Unwrap this {@link ThrowingDoubleToIntFunction}.
+     *
+     * @return this unwrapped {@link DoubleToIntFunction}
+     */
+    default DoubleToIntFunction unthrow() {
+        return this;
+    }
+}
